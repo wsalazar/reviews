811045bc6e68e0c47b356d666e76f83252579c3b
Mattrix22 <matthew.leon26@gmail.com>
2024-08-13 19:35:33 -0400

    Updates to index.html,App.jsx and index.css, giving them stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react';
-import data from './data'
+import data from './data';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const App = () => {
-  const [display, setDisplay] = useState(data[0])
-  const [reviewPerson, setReviewPerson] = useState(data)
+  const [display, setDisplay] = useState(data[0]);
+  const [reviewPerson, setReviewPerson] = useState(data);
 
   const previousReview = (id) => {
     /**
-     * using the setDisplay((currentReview) => {}) with modulus 
+     * using the setDisplay((currentReview) => {}) with modulus
      * gives us the current review. So we don't have to pass the id to get the current index.
      */
 
     setDisplay((currentReview) => {
-      let previousReviewIndex = ((currentReview.id - 1) - 1 + reviewPerson.length) % reviewPerson.length
+      let previousReviewIndex =
+        (currentReview.id - 1 - 1 + reviewPerson.length) % reviewPerson.length;
       if (previousReviewIndex < 0) {
         previousReviewIndex = reviewPerson.length - 1;
       }
       return reviewPerson[previousReviewIndex];
-    })
+    });
 
     /**
      * Without modulus and without using the currentReview in setDisplay()
@@ -30,46 +31,68 @@ const App = () => {
     // }
     // var previous = reviewPerson[previousIndex];
     // setDisplay(previous)
-  }
+  };
 
   const nextReview = (id) => {
     if (id > reviewPerson.length - 1) {
       const next = reviewPerson[0];
-      setDisplay(next)
+      setDisplay(next);
       return;
     }
     /**
      * With Modulus operator
      */
-    const nextIndex = (id % reviewPerson.length - 1) + 1
+    const nextIndex = (id % reviewPerson.length) - 1 + 1;
     const next = reviewPerson[nextIndex];
     /**
      * Without modules operator
      */
     // const nextIndex = typeof reviewPerson[id] === 'undefined' ? 0 : id++;
     // const next = reviewPerson[nextIndex];
-    setDisplay(next)
-  }
+    setDisplay(next);
+  };
 
   const randomReview = () => {
-    let randomIndex = Math.floor(Math.random() * ((reviewPerson.length-1) - 0 + 1) + 0);
+    let randomIndex = Math.floor(
+      Math.random() * (reviewPerson.length - 1 - 0 + 1) + 0
+    );
     const random = reviewPerson[randomIndex];
-    setDisplay(random)
-  }
+    setDisplay(random);
+  };
 
   return (
-  <div key={display.id}>
-    <article>Employee Review</article>
-      <img src={display.image} width="50px" height="50px"/>
-      <p>{display.name}</p>
-      <p>{display.job}</p>
-      <article>{display.text}</article>
-      <FaArrowLeft className='beer' onClick={() => previousReview(display.id)}/>
-      &nbsp;
-      <FaArrowRight className='beer' onClick={() => nextReview(display.id)}/>
+    <div className="review" key={display.id}>
+      <article>
+        <h5 className="title">Employee Review</h5>
+      </article>
+      <p className="quote-icon">"</p>
+      <div className="img-container">
+        <img
+          className="person-img"
+          src={display.image}
+          width="50px"
+          height="50px"
+        />
+      </div>
+      <p className="author">{display.name}</p>
+      <p className="job">{display.job}</p>
+      <article class="info">{display.text}</article>
+      <div className="btn-container">
+        <FaArrowLeft
+          className="prev-btn"
+          onClick={() => previousReview(display.id)}
+        />
+        &nbsp;
+        <FaArrowRight
+          className="next-btn"
+          onClick={() => nextReview(display.id)}
+        />
+      </div>
       <br />
-      <button className='btn' onClick={()=>randomReview()}>Random Person</button>
-  </div>
-  )
+      <button className="btn" onClick={() => randomReview()}>
+        Random Person
+      </button>
+    </div>
+  );
 };
 export default App;
